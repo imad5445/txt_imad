b21510ece660cbf7fe731f6369665671a857fabe
imad5445 <157965306+imad5445@users.noreply.github.com>
2024-02-11 19:56:12 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,3 +1,4 @@
-api_id = ""
-api_hash = ""
-bot_token = "" 
+api_id = "28088533"
+api_hash = "05533c10eb18ff30df25e3b7f77d49e6"
+bot_token = "6839667941:AAHVIKGNsKjwD-_24kvk49gcJIAVICW0NkA"
+sudo_group = -1001995230455 
